884bae1240e14eca0f7b264e8c61890f8a4855bd
Roman V <qwesc@yandex.ru>
2016-02-25 01:24:32 +0400

    login

--- blog/urls.py
+++ blog/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', login),
+    url(r'^accounts/logout/$', logout, {'next_page': '/'}),
     url(r'', include('blogpost.urls')),
 ]
--- blogpost/views.py
+++ blogpost/views.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
@@ -16,7 +18,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blogpost/post_detail.html', {'post': post})
 
-
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -30,7 +32,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blogpost/post_edit.html', {'form': form})
 
-
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -45,18 +47,18 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blogpost/post_edit.html', {'form': form})
 
-
+@login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blogpost/post_draft_list.html', {'posts': posts})
 
-
+@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_detail', pk=pk)
 
-
+@login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
