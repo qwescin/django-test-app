96d7c2a963d7ed737ef82807f92cec5fce34ee4e
Roman V <qwesc@yandex.ru>
2016-02-27 02:49:03 +0400

    added all templates to bootstrap

--- blogpost/views.py
+++ blogpost/views.py
@@ -10,7 +10,7 @@ from django.contrib.auth.decorators import login_required
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blogpost/post_list.html', {'posts': posts})
 
 
